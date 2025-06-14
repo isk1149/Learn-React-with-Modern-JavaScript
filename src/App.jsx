48310022d2e9b69a26a612d1384c8f33383b2ff9
Inseong Kim <isk1149@naver.com>
2025-06-14 21:30:55 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
 
-  const contentStyle = {
-    color: "orange",
+  const contentPinkStyle = {
+    color: "pink",
     fontSize: "20px",
   };
 
   return (
     <>
       <h1 style={{ color: "red" }}>안녕하세요.</h1>
-      <h2 style={contentStyle}>반갑습니다.</h2>
+      <ColoredMessage color="blue">잘 지내시죠???</ColoredMessage>
+      <ColoredMessage color="pink">잘 지냅니다!!!</ColoredMessage>
       <button onClick={onClickButton}>버튼</button>
     </>
   );
