a2441ae36ee5966862f524aea82389af3b4a1b81
Inseong Kim <isk1149@naver.com>
2025-06-17 21:54:01 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-import { ColoredMessage } from "./components/ColoredMessage";
-import { CssModules } from "./components/cssModules/CssModules";
+import { useState, memo } from "react";
+import { ColoredMessage } from "./components/04 Props, useState, useEffect/ColoredMessage";
+import { CssModules } from "./components/05 cssModules/CssModules";
+import { Child1 } from "./components/06 rerendering/Child1";
+import { Child4 } from "./components/06 rerendering/Child4";
+import { Button } from "./components/04 Props, useState, useEffect/Button";
 
-export const App = () => {
+export const App = memo(() => {
   const [num, setNum] = useState(0);
 
   const onClickButton = () => {
     setNum((prev) => prev + 1);
-  };
-
-  const contentPinkStyle = {
-    color: "pink",
-    fontSize: "20px",
   };
 
   return (
@@ -19,9 +17,12 @@ export const App = () => {
       <h1 style={{ color: "red" }}>안녕하세요.</h1>
       <ColoredMessage color="blue">잘 지내시죠???</ColoredMessage>
       <ColoredMessage color="pink">잘 지냅니다!!!</ColoredMessage>
-      <button onClick={onClickButton}>버튼</button>
+      <button onClick={onClickButton}>버튼-App.jsx</button>
       <p>{num}</p>
+      <Button>버튼-Button.jsx</Button>
       <CssModules />
+      <Child1 />
+      <Child4 />
     </>
   );
-};
+});
---
+++ src/components/04 Props, useState, useEffect/Button.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const Button = ({ children }) => {
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      <button onClick={onClickButton}>{children}</button>
+      <p>{num}</p>
+    </>
+  );
+};
